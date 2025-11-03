d0220864a1aaebc5f357c87b0685b61a84ab5988
Nax <4265415+Nax@users.noreply.github.com>
2025-11-03 03:13:15 +0100

    Cleanup

--- src/chantage/dllmain.c
+++ src/chantage/dllmain.c
@@ -43,7 +43,6 @@ static void setupHooks(void)
     void** pCreateProcessA;
 
     pCreateWindowExA = (void**)ResolveModulePtr(0x14060c840);
-    //CreateWindowExA_Original = (void*)*pCreateWindowExA;
     VirtualProtect(pCreateWindowExA, sizeof(void*), PAGE_EXECUTE_READWRITE, &oldProtect);
     *pCreateWindowExA = (void*)&CreateWindowExA_HOOK;
     VirtualProtect(pCreateWindowExA, sizeof(void*), oldProtect, &oldProtect);
