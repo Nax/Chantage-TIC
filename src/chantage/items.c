34f485a00637c97a28df48032ad2d1061c9851f3
Nax <4265415+Nax@users.noreply.github.com>
2025-11-07 00:47:44 +0100

    Pseudo-working

--- src/chantage/items.c
+++ src/chantage/items.c
@@ -1,5 +1,6 @@
 #include <chantage/chantage.h>
 #include <stdlib.h>
+#include <string.h>
 
 int sExtraItemCount;
 int sExtraItemCapacity;
@@ -27,6 +28,18 @@ ItemData* Item_GetData(uint16_t itemId)
     return table + itemId;
 }
 
+static void Item_PatchCount(void)
+{
+    uint16_t count;
+    uint8_t countLea;
+
+    count = 0x140 + sExtraItemCount;
+    countLea = count - 0xff;
+
+    /* Beware, this is probably a signed offset */
+    WriteProtectedRel8(0x280ba6, countLea);
+}
+
 uint16_t Item_Alloc(void)
 {
     uint16_t id;
@@ -37,8 +50,27 @@ uint16_t Item_Alloc(void)
         sExtraItems = realloc(sExtraItems, sizeof(ItemData) * sExtraItemCapacity);
     }
     id = 0x140 + sExtraItemCount;
+    memset(&sExtraItems[sExtraItemCount], 0, sizeof(ItemData));
     sExtraItemCount++;
+    Item_PatchCount();
     return id;
+}
+
+/* Should be changed into a mod eventually*/
+static void AddWotlItems(void)
+{
+    ItemData* item;
+    uint16_t itemId;
+
+    /* Vanguard helm */
+    itemId = Item_Alloc();
+    item = Item_GetData(itemId);
+    item->palette = 3;
+    item->gfx = 0x55;
+    item->flags = 0x22;
+    item->price = 10;
+    item->type = 0x14;
+    item->shop = 0x14;
 }
 
 void Init_Items(void)
@@ -47,5 +79,6 @@ void Init_Items(void)
     sExtraItemCapacity = 8;
     sExtraItems = malloc(sizeof(ItemData) * sExtraItemCapacity);
 
-    HookFunctionRel(0x2b4980, (void*)Item_GetData);
+    HookFunctionRel(0x02b4980, (void*)Item_GetData);
+    HookFunctionRel(0xe978db8, (void*)Item_GetCategory);
 }
