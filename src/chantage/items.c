966f245274b1112a8d9a67f8fa2853b51d419a83
Nax <4265415+Nax@users.noreply.github.com>
2025-11-07 21:40:39 +0100

    Sort of WIP

--- src/chantage/items.c
+++ src/chantage/items.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if 0
 static int sItemCount;
 static int sItemCapacity;
 static ItemExtendedData* sItems;
@@ -182,9 +183,9 @@ static void LoadItems(void)
     srcItems = BaseRelPtr(0x808740);
     for (int i = 0; i < 0x100; ++i)
         memcpy(&sItems[i].base, &srcItems[i], sizeof(ItemData));
-    //srcItems = BaseRelPtr(0x67a870);
-    //for (int i = 0x100; i < 0x105; ++i)
-    //    memcpy(&sItems[i].base, &srcItems[i - 0x100], sizeof(ItemData));
+    srcItems = BaseRelPtr(0x67b470);
+    for (int i = 0; i < 5; ++i)
+        memcpy(&sItems[i + 0x100].base, &srcItems[i], sizeof(ItemData));
 }
 
 void Init_Items(void)
@@ -194,9 +195,105 @@ void Init_Items(void)
 
     HookFunctionRel(0x02b4980, (void*)Item_GetData);
     HookFunctionRel(0xe978db8, (void*)Item_GetCategory);
-    HookFunctionRel(0xe9983d0, (void*)Item_GetWeaponData);
-    HookFunctionRel(0xe99fff0, (void*)Item_GetShieldData);
-    HookFunctionRel(0xe9da940, (void*)Item_GetChemistData);
-    HookFunctionRel(0xe9bae08, (void*)Item_GetArmorData);
-    HookFunctionRel(0xe9d490e, (void*)Item_GetAccessoryData);
+    //HookFunctionRel(0xe9983d0, (void*)Item_GetWeaponData);
+    //HookFunctionRel(0xe99fff0, (void*)Item_GetShieldData);
+    //HookFunctionRel(0xe9da940, (void*)Item_GetChemistData);
+    //HookFunctionRel(0xe9bae08, (void*)Item_GetArmorData);
+    //HookFunctionRel(0xe9d490e, (void*)Item_GetAccessoryData);
+}
+#endif
+
+static ItemData sItems[256];
+
+static void LoadItems(void)
+{
+    ItemData* srcItems;
+
+    /* Copy the existing items */
+    srcItems = BaseRelPtr(0x808740);
+    for (int i = 0; i < 0x100; ++i)
+        memcpy(&sItems[i], &srcItems[i], sizeof(ItemData));
+    srcItems = BaseRelPtr(0x67b470);
+    for (int i = 0; i < 5; ++i)
+        memcpy(&sItems[i + 0x100], &srcItems[i], sizeof(ItemData));
+}
+
+static ItemData* Item_GetData(uint16_t itemId)
+{
+    if (itemId >= 0x105)
+        return NULL;
+    return &sItems[itemId];
+}
+
+int Item_GetCategory(uint16_t itemId)
+{
+    ItemData* item;
+
+    if (itemId == 0xfe || itemId == 0xff)
+        return ITEM_CATEGORY_NONE;
+    item = Item_GetData(itemId);
+    if (!item)
+        return ITEM_CATEGORY_NONE;
+
+    switch (item->type)
+    {
+    case ITEM_TYPE_NONE:
+    case ITEM_TYPE_KNIFE:
+    case ITEM_TYPE_NINJABLADE:
+    case ITEM_TYPE_SWORD:
+    case ITEM_TYPE_KNIGHTSWORD:
+    case ITEM_TYPE_KATANA:
+    case ITEM_TYPE_AXE:
+    case ITEM_TYPE_ROD:
+    case ITEM_TYPE_STAFF:
+    case ITEM_TYPE_FLAIL:
+    case ITEM_TYPE_GUN:
+    case ITEM_TYPE_CROSSBOW:
+    case ITEM_TYPE_BOW:
+    case ITEM_TYPE_INSTRUMENT:
+    case ITEM_TYPE_BOOK:
+    case ITEM_TYPE_POLEARM:
+    case ITEM_TYPE_POLE:
+    case ITEM_TYPE_BAG:
+    case ITEM_TYPE_CLOTH:
+        return ITEM_CATEGORY_WEAPON;
+    case ITEM_TYPE_THROWING:
+    case ITEM_TYPE_BOMB:
+        return ITEM_CATEGORY_THROW;
+    case ITEM_TYPE_SHIELD:
+        return ITEM_CATEGORY_SHIELD;
+    case ITEM_TYPE_HELMET:
+    case ITEM_TYPE_HAT:
+    case ITEM_TYPE_HAIRADORN:
+        return ITEM_CATEGORY_HELM;
+    case ITEM_TYPE_ARMOR:
+    case ITEM_TYPE_CLOTHING:
+    case ITEM_TYPE_ROBE:
+        return ITEM_CATEGORY_ARMOR;
+    case ITEM_TYPE_SHOES:
+    case ITEM_TYPE_ARMGUARD:
+    case ITEM_TYPE_RING:
+    case ITEM_TYPE_ARMLET:
+    case ITEM_TYPE_CLOAK:
+    case ITEM_TYPE_PERFUME:
+        return ITEM_CATEGORY_ACCESSORY;
+    case ITEM_TYPE_CHEMIST:
+        return ITEM_CATEGORY_CHEMIST;
+    default:
+        return ITEM_CATEGORY_NONE;
+    }
+}
+
+void Init_Items(void)
+{
+    LoadItems();
+    //AddWotlItems();
+
+    HookFunctionRel(0x02b4980, (void*)Item_GetData);
+    HookFunctionRel(0xe978db8, (void*)Item_GetCategory);
+    //HookFunctionRel(0xe9983d0, (void*)Item_GetWeaponData);
+    //HookFunctionRel(0xe99fff0, (void*)Item_GetShieldData);
+    //HookFunctionRel(0xe9da940, (void*)Item_GetChemistData);
+    //HookFunctionRel(0xe9bae08, (void*)Item_GetArmorData);
+    //HookFunctionRel(0xe9d490e, (void*)Item_GetAccessoryData);
 }
