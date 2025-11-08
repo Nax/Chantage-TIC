aa4e33061e4aee3585a79f53e89053d013b74b04
Nax <4265415+Nax@users.noreply.github.com>
2025-11-08 21:36:05 +0100

    Good console

--- src/chantage/console.c
+++ src/chantage/console.c
@@ -5,14 +5,70 @@ extern HWND gGameWindow;
 static ATOM sConsoleClass;
 static HWND sConsoleWindow;
 
+static LRESULT CALLBACK Console_WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            RECT rect;
+            GetClientRect(hwnd, &rect);
+
+            // Fill with 50% gray background
+            HBRUSH grayBrush = CreateSolidBrush(RGB(128, 128, 128));
+            FillRect(hdc, &rect, grayBrush);
+            DeleteObject(grayBrush);
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        case WM_KEYDOWN:
+        {
+            // Handle keyboard input here
+            char keyChar = (char)wParam;
+            if (keyChar >= 32 && keyChar <= 126) // Printable characters
+            {
+                // TODO: Add character to input buffer
+            }
+            else if (wParam == VK_RETURN)
+            {
+                // TODO: Execute lua command
+            }
+            else if (wParam == VK_BACK)
+            {
+                // TODO: Remove last character from input buffer
+            }
+            return 0;
+        }
+        case WM_CHAR:
+        {
+            // Handle character input
+            char ch = (char)wParam;
+            if (ch >= 32 && ch <= 126) // Printable ASCII
+            {
+                // TODO: Add to input buffer and redraw
+            }
+            return 0;
+        }
+        case WM_DESTROY:
+            sConsoleWindow = NULL;
+            return 0;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+}
+
 static void Console_RegisterClass(void)
 {
-    /* Transparent window */
     WNDCLASS wc = {0};
-    wc.lpfnWndProc = DefWindowProc;
+    wc.lpfnWndProc = Console_WndProc;
     wc.hInstance = GetModuleHandleA(NULL);
     wc.lpszClassName = "ConsoleClass";
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hbrBackground = NULL; // No automatic background brush
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
     sConsoleClass = RegisterClassA(&wc);
 }
 
@@ -21,25 +77,48 @@ static void Console_Open(void)
     if (!gGameWindow)
         return;
 
+    // Get game window dimensions
+    RECT gameRect;
+    GetWindowRect(gGameWindow, &gameRect);
+
+    int consoleWidth = (gameRect.right - gameRect.left);
+    int consoleHeight = (gameRect.bottom - gameRect.top);
+    int consoleX = gameRect.left;
+    int consoleY = gameRect.top;
+
     sConsoleWindow = CreateWindowExA(
-        WS_EX_TOPMOST,
+        WS_EX_LAYERED,
         "ConsoleClass",
-        "Lua Console",
-        WS_OVERLAPPEDWINDOW,
-        100, 100, 800, 600,
+        NULL, // No title
+        WS_POPUP, // Borderless
+        consoleX, consoleY, consoleWidth, consoleHeight,
         gGameWindow,
         NULL,
         GetModuleHandleA(NULL),
         NULL
     );
 
-    ShowWindow(sConsoleWindow, SW_SHOW);
+    if (sConsoleWindow)
+    {
+        // Set 50% transparency (128 out of 255)
+        SetLayeredWindowAttributes(sConsoleWindow, 0, 128, LWA_ALPHA);
+        ShowWindow(sConsoleWindow, SW_SHOW);
+        SetFocus(sConsoleWindow); // Steal focus from game
+        UpdateWindow(sConsoleWindow);
+    }
 }
 
 static void Console_Close(void)
 {
-    DestroyWindow(sConsoleWindow);
-    sConsoleWindow = NULL;
+    if (sConsoleWindow)
+    {
+        DestroyWindow(sConsoleWindow);
+        sConsoleWindow = NULL;
+
+        // Return focus to the game
+        if (gGameWindow)
+            SetFocus(gGameWindow);
+    }
 }
 
 void Console_Toggle(void)
@@ -58,6 +137,17 @@ LRESULT CALLBACK ConsoleKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
         if (wParam == WM_KEYDOWN && p->vkCode == VK_OEM_3)
         {
             Console_Toggle();
+            return 1;
+        }
+
+        if (sConsoleWindow && IsWindowVisible(sConsoleWindow))
+        {
+            // Forward the key to the console window
+            if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+            {
+                PostMessage(sConsoleWindow, WM_KEYDOWN, p->vkCode, 0);
+            }
+            return 1;
         }
     }
     return CallNextHookEx(NULL, nCode, wParam, lParam);
